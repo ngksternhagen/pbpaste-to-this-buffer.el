147b5e45832cfa4998b36073f1f651be9dc51184
NGK Sternhagen <sternhagen@protonmail.sh>
2017-01-05 14:07:12 -0800

    add package boilerplate

--- pbpaste-to-this-buffer.el
+++ pbpaste-to-this-buffer.el
@@ -1,3 +1,10 @@
+;;; pbpaste-to-this-buffer.el --- paste to the current buffer using pbpaste
+
+;;; Version: 0.0.0
+;;; Author: NGK Sternhagen <sternhagen@protonmail.ch>
+;;; Keywords: pbpaste
+;;; Created: 05 January 2017
+
 (defun append-string-to-buffer (string buffer)
   "Append STRING to the end of BUFFER."
   (with-current-buffer buffer
@@ -11,3 +18,7 @@
   (append-string-to-buffer (shell-command-to-string "pbpaste") (current-buffer)))
 
 ;; TODO handle case where current buffer is remote, such as TRAMP, ssh, etc
+
+(provide 'pbpaste-to-this-buffer)
+
+;;; pbpaste-to-this-buffer.el ends here
